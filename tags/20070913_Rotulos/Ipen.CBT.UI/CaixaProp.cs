86aef011052cd03331ae5e2d4cfaff8f1a68ede7
T.Claro <T.Claro@080f19af-0837-0410-8be1-f191de51b343>
2007-09-14 01:29:49 +0000

    made a copy

---
+++ tags/20070913_Rotulos/Ipen.CBT.UI/CaixaProp.cs
@@ -0,0 +1,294 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Ipen.CompartimentalModel;
+
+namespace Ipen.CBT.UI
+{
+	/// <summary>
+	/// Summary description for CaixaProp.
+	/// </summary>
+	public class CaixaProp : System.Windows.Forms.Form
+	{
+		#region Campos
+        private Caixas _caixaPropriedades;
+        private int Numero;
+        private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btnCor;
+		private System.Windows.Forms.Button btnCancelar;
+		private System.Windows.Forms.Button btnOK;
+		private System.Windows.Forms.Label lblNome;
+		private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.ColorDialog dlgCor;
+        private System.Windows.Forms.CheckBox chkAcompanhar;
+        private CheckBox chkEliminacao;
+        private CheckBox chkIncorporacao;
+        private Label lblFracao;
+        private TextBox txtFracao;
+        
+		private System.ComponentModel.Container components = null;
+		#endregion
+
+		#region Construtor
+		public CaixaProp(string Nome, System.Drawing.Color CorDaLinha, bool Acompanhar, bool Eliminacao)
+		{
+			InitializeComponent();
+			this.txtNome.Text = Nome;
+			this.btnCor.BackColor = CorDaLinha;
+            chkAcompanhar.Checked = Acompanhar;
+            chkEliminacao.Checked = Eliminacao;
+		}
+		public CaixaProp(Caixas cx)
+		{
+			InitializeComponent();
+            this._caixaPropriedades = cx;
+            if (_caixaPropriedades == null)
+                _caixaPropriedades = new Caixas("[Sem Título]", Caixas.CorPadrao, false, false);
+
+            PreencherRegistro();
+ 		}
+        private void PreencherRegistro()
+        {
+            this.txtNome.Text = _caixaPropriedades.Nome;
+            Numero = _caixaPropriedades.Numero;
+            this.dlgCor.Color = _caixaPropriedades.BackColor;
+            this.btnCor.BackColor = this.dlgCor.Color;
+            this.chkAcompanhar.Checked = _caixaPropriedades.Acompanhar;
+            this.chkEliminacao.Checked = _caixaPropriedades.Eliminacao;
+            this.chkIncorporacao.Checked = _caixaPropriedades.Incorporacao;
+            this.txtFracao.Text = _caixaPropriedades.Fracao.ToString();
+        }
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if(components != null)
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCor = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.dlgCor = new System.Windows.Forms.ColorDialog();
+            this.chkAcompanhar = new System.Windows.Forms.CheckBox();
+            this.chkEliminacao = new System.Windows.Forms.CheckBox();
+            this.chkIncorporacao = new System.Windows.Forms.CheckBox();
+            this.lblFracao = new System.Windows.Forms.Label();
+            this.txtFracao = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(195, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Cor:";
+            // 
+            // btnCor
+            // 
+            this.btnCor.Location = new System.Drawing.Point(195, 24);
+            this.btnCor.Name = "btnCor";
+            this.btnCor.Size = new System.Drawing.Size(32, 21);
+            this.btnCor.TabIndex = 1;
+            this.btnCor.Click += new System.EventHandler(this.btnCor_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.Location = new System.Drawing.Point(145, 117);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 4;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(65, 117);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(72, 24);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "OK";
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // lblNome
+            // 
+            this.lblNome.Location = new System.Drawing.Point(8, 8);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(176, 16);
+            this.lblNome.TabIndex = 9;
+            this.lblNome.Text = "Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(8, 24);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(181, 21);
+            this.txtNome.TabIndex = 0;
+            // 
+            // chkAcompanhar
+            // 
+            this.chkAcompanhar.AutoSize = true;
+            this.chkAcompanhar.Location = new System.Drawing.Point(8, 60);
+            this.chkAcompanhar.Name = "chkAcompanhar";
+            this.chkAcompanhar.Size = new System.Drawing.Size(86, 17);
+            this.chkAcompanhar.TabIndex = 2;
+            this.chkAcompanhar.Text = "Acompanhar";
+            this.chkAcompanhar.UseVisualStyleBackColor = true;
+            // 
+            // chkEliminacao
+            // 
+            this.chkEliminacao.AutoSize = true;
+            this.chkEliminacao.Location = new System.Drawing.Point(8, 83);
+            this.chkEliminacao.Name = "chkEliminacao";
+            this.chkEliminacao.Size = new System.Drawing.Size(75, 17);
+            this.chkEliminacao.TabIndex = 12;
+            this.chkEliminacao.Text = "Eliminação";
+            this.chkEliminacao.UseVisualStyleBackColor = true;
+            // 
+            // chkIncorporacao
+            // 
+            this.chkIncorporacao.AutoSize = true;
+            this.chkIncorporacao.Location = new System.Drawing.Point(107, 60);
+            this.chkIncorporacao.Name = "chkIncorporacao";
+            this.chkIncorporacao.Size = new System.Drawing.Size(90, 17);
+            this.chkIncorporacao.TabIndex = 13;
+            this.chkIncorporacao.Text = "Incorporacao";
+            this.chkIncorporacao.UseVisualStyleBackColor = true;
+            // 
+            // lblFracao
+            // 
+            this.lblFracao.AutoSize = true;
+            this.lblFracao.Location = new System.Drawing.Point(104, 84);
+            this.lblFracao.Name = "lblFracao";
+            this.lblFracao.Size = new System.Drawing.Size(40, 13);
+            this.lblFracao.TabIndex = 14;
+            this.lblFracao.Text = "Fração";
+            // 
+            // txtFracao
+            // 
+            this.txtFracao.Location = new System.Drawing.Point(150, 81);
+            this.txtFracao.Name = "txtFracao";
+            this.txtFracao.Size = new System.Drawing.Size(53, 21);
+            this.txtFracao.TabIndex = 15;
+            // 
+            // CaixaProp
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 14);
+            this.CancelButton = this.btnCancelar;
+            this.ClientSize = new System.Drawing.Size(232, 152);
+            this.Controls.Add(this.txtFracao);
+            this.Controls.Add(this.lblFracao);
+            this.Controls.Add(this.chkIncorporacao);
+            this.Controls.Add(this.chkEliminacao);
+            this.Controls.Add(this.chkAcompanhar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCor);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txtNome);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "CaixaProp";
+            this.Opacity = 0.85;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Caixa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+		#endregion
+		#endregion
+
+		#region Métodos de Eventos
+		private void btnCor_Click(object sender, System.EventArgs e)
+		{
+			this.dlgCor.Color = this.btnCor.BackColor;
+			this.dlgCor.ShowDialog();
+			this.btnCor.BackColor = this.dlgCor.Color;
+		}
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this._caixaPropriedades.Nome = this.txtNome.Text;
+            this._caixaPropriedades.BackColor = this.dlgCor.Color;
+            this._caixaPropriedades.Acompanhar = this.chkAcompanhar.Checked;
+            this._caixaPropriedades.Eliminacao = this.chkEliminacao.Checked;
+            this._caixaPropriedades.Incorporacao = this.chkIncorporacao.Checked;
+            this._caixaPropriedades.Fracao = double.Parse(this.txtFracao.Text);
+        }
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+		#region Acesso aos Campos
+        public int NumeroDaCaixa
+        {
+            get
+            {
+                return Numero;
+            }
+        }
+		public string NomeDaCaixa
+		{
+			get
+			{
+				if (this.txtNome.Text.Trim().Length > 0)
+					return this.txtNome.Text.Trim();
+				else
+					return "[Sem título]";
+			}
+		}
+		public System.Drawing.Color CorDaCaixa
+		{
+			get
+			{
+				return this.btnCor.BackColor;
+			}
+		}
+
+        public bool Acompanhar
+        {
+            get
+            {
+                return this.chkAcompanhar.Checked;
+            }
+        }
+
+        public bool Eliminacao
+        {
+            get
+            {
+                return this.chkEliminacao.Checked;
+            }
+        }
+        public Caixas CaixaPropriedades
+        {
+            get { return _caixaPropriedades; }
+            set { _caixaPropriedades = value; }
+        }
+
+		#endregion
+	}
+}
---
+++ tags/20070913_Rotulos/Ipen.CBT.UI/Painel.cs
@@ -0,0 +1,280 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using Ipen.CompartimentalModel;
+
+namespace Ipen.CBT.UI
+{
+    public class Painel : Panel
+    {
+        #region Campos
+        Sistema _sistemaCompartimental;
+        #endregion
+
+        #region Construtor
+        public Painel()
+        {
+            InitializeHelper();
+        }
+        void InitializeHelper()
+        {
+            _sistemaCompartimental = new Sistema();
+            _sistemaCompartimental.BoxMouseDown += new MouseEventHandler(_sistemaCompartimental_BoxMouseDown);
+            _sistemaCompartimental.BoxDeleted += new Caixas.CaixaEventHandler(_sistemaCompartimental_BoxDeleted);
+            _sistemaCompartimental.BoxDoubleClick += new EventHandler(_sistemaCompartimental_BoxDoubleClick);
+            _sistemaCompartimental.BoxKeyDown += new KeyEventHandler(_sistemaCompartimental_BoxKeyDown);
+            _sistemaCompartimental.BoxMoved += new Caixas.CaixaEventHandler(_sistemaCompartimental_BoxMoved);
+            _sistemaCompartimental.BoxPropertyChanged += new Caixas.CaixaEventHandler(_sistemaCompartimental_BoxPropertyChanged);
+            _sistemaCompartimental.BoxMove += new EventHandler(_sistemaCompartimental_BoxMove);
+        }
+        #endregion
+
+        #region Propriedades públicas
+        public Sistema SistemaCompartimental
+        {
+            get { return _sistemaCompartimental; }
+            set { _sistemaCompartimental = value; }
+        }
+        #endregion
+
+        #region Métodos públicos
+        public void IncluirCaixa(Caixas cx)
+        {
+            this._sistemaCompartimental.Caixas.Add(cx);
+            this.Controls.Add(cx);
+            cx.BringToFront();
+            this.VerificarCaixasSobrepostas(cx);
+            this.Refresh();
+        }
+        public void IncluirLinha(Linhas ln)
+        {
+            //ln.Redesenhar = true;
+            this._sistemaCompartimental.Linhas.Add(ln);
+            this.Refresh();
+        }
+        public void DesmarcarTudo()
+        {
+            foreach (Caixas cx in this._sistemaCompartimental.Caixas)
+                cx.EstaSelecionado = false;
+
+            foreach (Linhas ln in this._sistemaCompartimental.Linhas)
+                ln.EstaSelecionado = false;
+        }
+        #endregion
+
+        #region Novos Eventos
+        public delegate void BoxModifyRequestHandle(Caixas cx);
+        public event BoxModifyRequestHandle BoxModifyRequest;
+        protected void OnBoxModifyRequest(Caixas cx)
+        {
+            if (BoxModifyRequest != null)
+                BoxModifyRequest(cx);
+        }
+        #endregion
+
+        #region Métodos de eventos
+        void _sistemaCompartimental_BoxPropertyChanged(Caixas.BoxEventArgs be)
+        {
+            this.Refresh();
+            this.VerificarCaixasSobrepostas(be.Box);
+        }
+        void _sistemaCompartimental_BoxMove(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+        void _sistemaCompartimental_BoxMoved(Caixas.BoxEventArgs be)
+        {
+            this.VerificarCaixasSobrepostas(be.Box);
+        }
+        void _sistemaCompartimental_BoxKeyDown(object sender, KeyEventArgs e)
+        {
+            if (!(sender is Caixas))
+                return;
+            Caixas cx = (Caixas)sender;
+            if (e.KeyCode == Keys.Delete)
+            {
+                DialogResult dlgResposta = MessageBox.Show(string.Format("Tem certeza que deseja excluir o compartimento {0} ({1}) e todas as suas ligações?", cx.Numero, cx.Nome), "Exclusão de compartimento", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+                if (dlgResposta == DialogResult.Yes)
+                    this._sistemaCompartimental.Caixas.Remove(cx);
+            }
+        }
+        void _sistemaCompartimental_BoxDoubleClick(object sender, EventArgs e)
+        {
+            if (!(sender is Caixas))
+                return;
+            Caixas cx = (Caixas)sender;
+            OnBoxModifyRequest(cx);
+        }
+        void _sistemaCompartimental_BoxDeleted(Caixas.BoxEventArgs be)
+        {
+        }
+        void _sistemaCompartimental_BoxMouseDown(object sender, MouseEventArgs e)
+        {
+            this.DesmarcarTudo();
+
+            if (!(sender is Caixas))
+                return;
+            Caixas cx = (Caixas)sender;
+            cx.BringToFront();
+            cx.EstaSelecionado = true;
+        }
+        protected override void OnClick(EventArgs e)
+        {
+            this.DesmarcarTudo();
+            base.OnClick(e);
+        }
+        #endregion
+
+        #region Métodos internos
+        private void VerificarCaixasSobrepostas(Caixas cx)
+        {
+            while (ObterCaixaPorClique(cx.PontosExtremos, cx) != null)
+                cx.Left += 5;
+        }
+        private Caixas ObterCaixaPorClique(System.Drawing.Point pto)
+        {
+            foreach (Caixas cx in this._sistemaCompartimental.Caixas)
+                if (cx.PontoNessaCaixa(pto))
+                    return cx;
+
+            return null;
+        }
+        private Caixas ObterCaixaPorClique(System.Drawing.Point[] ptos, Caixas CaixaNaoVerificada)
+        {
+            foreach (Caixas cx in this._sistemaCompartimental.Caixas)
+            {
+                if (cx == CaixaNaoVerificada)
+                    continue;
+                foreach (System.Drawing.Point pto in ptos)
+                {
+                    if (cx.PontoNessaCaixa(pto))
+                        return cx;
+                }
+                System.Drawing.Point[] ptos2 = cx.PontosExtremos;
+                foreach (System.Drawing.Point pto in ptos2)
+                {
+                    if (CaixaNaoVerificada.PontoNessaCaixa(pto))
+                        return cx;
+                }
+            }
+
+            return null;
+        }
+        #endregion
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            #region Configurar gráficos
+            //e.Graphics.CompositingQuality = CompositingQuality.HighQuality;
+            //e.Graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            //e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            //e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            #endregion
+            
+            #region Desenhar Linhas
+            foreach (Linhas ln in this.SistemaCompartimental.Linhas)
+            {
+                if (!ln.Redesenhar)
+                   //continue;
+
+                //Desenha a seta
+                if (ln.DirecaoDaLinha == Linhas.Direcao.InicioParaFim || ln.DirecaoDaLinha == Linhas.Direcao.Ambos)
+                    DesenharSetaDirecao(ln.CaixaFim, ln, ln.CaixaInicio, e.Graphics);
+                if (ln.DirecaoDaLinha == Linhas.Direcao.FimParaInicio || ln.DirecaoDaLinha == Linhas.Direcao.Ambos)
+                    DesenharSetaDirecao(ln.CaixaInicio, ln, ln.CaixaFim, e.Graphics);
+
+                //Desenha a linha
+                e.Graphics.DrawLine(new Pen(ln.ForeColor), ln.PontoInicio, ln.PontoFim);
+
+                //Label da Linha
+                if (Configuracoes.ExibirRotulos)
+                {
+                    if (ln.DirecaoDaLinha != Linhas.Direcao.Ambos)
+                    {
+                        System.Drawing.SizeF tamanho = e.Graphics.MeasureString(ln.Nome, ln.Font);
+                        e.Graphics.FillRectangle(System.Drawing.Brushes.White, (ln.PontoCentral.X - tamanho.Width / 2) - 1, (ln.PontoCentral.Y - tamanho.Height / 2) - 1, tamanho.Width + 2, tamanho.Height + 2);
+                        e.Graphics.DrawString(ln.Nome, ln.Font, new System.Drawing.SolidBrush(ln.ForeColor), ln.PontoCentral.X - tamanho.Width / 2, ln.PontoCentral.Y - tamanho.Height / 2);
+                        e.Graphics.DrawRectangle(new Pen(ln.ForeColor), (ln.PontoCentral.X - tamanho.Width / 2) - 1, (ln.PontoCentral.Y - tamanho.Height / 2) - 1, tamanho.Width + 2, tamanho.Height + 2);
+                    }
+                    else
+                    {
+                        System.Drawing.SizeF tamanho = e.Graphics.MeasureString(ln.NomeAB, ln.Font);
+                        e.Graphics.FillRectangle(System.Drawing.Brushes.White, (ln.PontoTercoInicio.X - tamanho.Width / 2) - 1, (ln.PontoTercoInicio.Y - tamanho.Height / 2) - 1, tamanho.Width + 2, tamanho.Height + 2);
+                        e.Graphics.DrawString(ln.NomeAB, ln.Font, new System.Drawing.SolidBrush(ln.ForeColor), ln.PontoTercoInicio.X - tamanho.Width / 2, ln.PontoTercoInicio.Y - tamanho.Height / 2);
+                        e.Graphics.DrawRectangle(new Pen(ln.ForeColor), (ln.PontoTercoInicio.X - tamanho.Width / 2) - 1, (ln.PontoTercoInicio.Y - tamanho.Height / 2) - 1, tamanho.Width + 2, tamanho.Height + 2);
+
+                        System.Drawing.SizeF tamanho2 = e.Graphics.MeasureString(ln.NomeBA, ln.Font);
+                        e.Graphics.FillRectangle(System.Drawing.Brushes.White, (ln.PontoTercoFim.X - tamanho2.Width / 2) - 1, (ln.PontoTercoFim.Y - tamanho2.Height / 2) - 1, tamanho2.Width + 2, tamanho2.Height + 2);
+                        e.Graphics.DrawString(ln.NomeBA, ln.Font, new System.Drawing.SolidBrush(ln.ForeColor), ln.PontoTercoFim.X - tamanho2.Width / 2, ln.PontoTercoFim.Y - tamanho2.Height / 2);
+                        e.Graphics.DrawRectangle(new Pen(ln.ForeColor), (ln.PontoTercoFim.X - tamanho2.Width / 2) - 1, (ln.PontoTercoFim.Y - tamanho2.Height / 2) - 1, tamanho2.Width + 2, tamanho2.Height + 2);
+                    }
+                }
+            }
+            #endregion
+                   
+            //base.OnPaint(e);
+        }
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            base.OnPaintBackground(e);
+        }
+        
+        private void DesenharSetaDirecao(Caixas Caixa, Linhas ln, Caixas OutraCaixa, Graphics g)
+        {
+            //Seta na parte superior do compartimento
+            if (ln.XdeY(Caixa.Top) >= Caixa.Left && ln.XdeY(Caixa.Top) <= Caixa.Right && OutraCaixa.PontoCentral.Y < Caixa.PontoCentral.Y)
+            {
+                Point pontaDaSeta = new Point(ln.XdeY(Caixa.Top), Caixa.Top);
+                Point verticeDireita = new Point(pontaDaSeta.X + 4, pontaDaSeta.Y - 14);
+                Point verticeEsquerda = new Point(pontaDaSeta.X - 4, pontaDaSeta.Y - 14);
+                GraphicsPath setaCompleta = new GraphicsPath();
+                setaCompleta.AddLine(pontaDaSeta, verticeDireita);
+                setaCompleta.AddLine(verticeDireita, verticeEsquerda);
+                setaCompleta.AddLine(verticeEsquerda, pontaDaSeta);
+                g.FillPath(new SolidBrush(ln.ForeColor), setaCompleta);
+            }
+
+            //Seta na parte inferior 
+            if (ln.XdeY(Caixa.Top) >= Caixa.Left && ln.XdeY(Caixa.Top) <= Caixa.Right && OutraCaixa.PontoCentral.Y > Caixa.PontoCentral.Y)
+            {
+                Point pontaDaSeta = new Point(ln.XdeY(Caixa.Bottom), Caixa.Bottom);
+                Point verticeDireita = new Point(pontaDaSeta.X + 4, pontaDaSeta.Y + 14);
+                Point verticeEsquerda = new Point(pontaDaSeta.X - 4, pontaDaSeta.Y + 14);
+                GraphicsPath setaCompleta = new GraphicsPath();
+                setaCompleta.AddLine(pontaDaSeta, verticeDireita);
+                setaCompleta.AddLine(verticeDireita, verticeEsquerda);
+                setaCompleta.AddLine(verticeEsquerda, pontaDaSeta);
+                g.FillPath(new SolidBrush(ln.ForeColor), setaCompleta);
+            }
+
+            //Seta na lateral esquerda
+            if (ln.YdeX(Caixa.Left) >= Caixa.Top && ln.YdeX(Caixa.Left) <= Caixa.Bottom && OutraCaixa.PontoCentral.X < Caixa.PontoCentral.X)
+            {
+                Point pontaDaSeta = new Point(Caixa.Left, ln.YdeX(Caixa.Left));
+                Point verticeDireita = new Point(pontaDaSeta.X - 14, pontaDaSeta.Y + 4);
+                Point verticeEsquerda = new Point(pontaDaSeta.X - 14, pontaDaSeta.Y - 4);
+                GraphicsPath setaCompleta = new GraphicsPath();
+                setaCompleta.AddLine(pontaDaSeta, verticeDireita);
+                setaCompleta.AddLine(verticeDireita, verticeEsquerda);
+                setaCompleta.AddLine(verticeEsquerda, pontaDaSeta);
+                g.FillPath(new SolidBrush(ln.ForeColor), setaCompleta);
+            }
+
+            //Seta na lateral direita
+            if (ln.YdeX(Caixa.Left) >= Caixa.Top && ln.YdeX(Caixa.Left) <= Caixa.Bottom && OutraCaixa.PontoCentral.X > Caixa.PontoCentral.X)
+            {
+                Point pontaDaSeta = new Point(Caixa.Right, ln.YdeX(Caixa.Right));
+                Point verticeDireita = new Point(pontaDaSeta.X + 14, pontaDaSeta.Y + 4);
+                Point verticeEsquerda = new Point(pontaDaSeta.X + 14, pontaDaSeta.Y - 4);
+                GraphicsPath setaCompleta = new GraphicsPath();
+                setaCompleta.AddLine(pontaDaSeta, verticeDireita);
+                setaCompleta.AddLine(verticeDireita, verticeEsquerda);
+                setaCompleta.AddLine(verticeEsquerda, pontaDaSeta);
+                g.FillPath(new SolidBrush(ln.ForeColor), setaCompleta);
+            }
+        }
+    }
+}
